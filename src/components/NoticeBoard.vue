97846118f532df7443c284cea1c8495916a03eca
area123 <kjs0713kjs@naver.com>
2020-09-18 15:49:41 +0900

    공지노트 작성

---
+++ src/components/NoticeBoard.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .column.card
+    header.card-header
+      a(href="#").card-header-title.has-text-centered 공지사항
+    .card-content
+      ul
+        li(v-for='n in 10') 제목
+</template>
+
+<script>
+export default {
+  name: 'NoticeBoard',
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+  display: block;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,21 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  div
+    .columns
+      NoticeBoard
+      NoticeBoard
+    .columns
+      NoticeBoard
+      NoticeBoard
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from 'vue';
+import NoticeBoard from '@/components/NoticeBoard.vue';
 
-@Component({
-  components: {
-    HelloWorld,
+export default Vue.extend({
+  components: { NoticeBoard },
+  data() {
+
   },
-})
-export default class Home extends Vue {}
+});
 </script>
